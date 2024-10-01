f728180aeedd5957a022ce47751a3b8ed99042ca
yashmalai <ilnaz204@yandex.ru>
2024-10-02 00:43:01 +0500

    ready

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import smtplib
 import os
 import dotenv
+import imapclient
+import pyzmail
+
 # from email.mime.text import MIMEText
 # from email.mime.multipart import MIMEMultipart
 
 dotenv.load_dotenv()
 
-smtp  = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
-# smtp.starttls()
-print("Введите логин")
-smtp.login(f'{os.getenv('EMAIL')}', f'{os.getenv('PASS')}')
-smtp.ehlo()
+# smtp  = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+# # smtp.starttls()
+# smtp.login(f'{os.getenv('EMAIL')}', f'{os.getenv('PASS')}')
+# print(smtp.ehlo())
 
+imap = imapclient.IMAPClient('imap.yandex.ru', ssl=True)
+imap.login(os.getenv('EMAIL'), os.getenv('PASS'))
+
+imap.logout()
